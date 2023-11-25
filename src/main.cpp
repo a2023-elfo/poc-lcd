5c02adf88fe62b12d268ee0270cdb40afb5fa28f
Jordan Gagnon <itsseraphii@gmail.com>
2023-11-25 16:23:59 -0500

    Hello, ELFO!
    Works on Arduino UNO

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
-#include <LibRobus.h>
+#include <LiquidCrystal.h>
+
+const int rs = 3, en = 4, d4 = 5, d5 = 6, d6 = 7, d7 = 8;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  BoardInit();
+  lcd.begin(16, 2);
   Serial.begin(9600);
-  Serial.println("Hello World!");
+  lcd.setCursor(2, 0);
+  lcd.print("Hello, ELFO!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lcd.setCursor(0, 1);
+
+  // print the number of seconds since reset:
+
+  lcd.print(millis() / 1000);
 }
